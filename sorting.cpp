570a5bf6d8604025f88fec85e144c91cf6da58d6
JAYash9 <jayash.shrivastav@gmail.com>
2025-01-08 12:18:20 +0530

    added bubble sort

---
+++ sorting.cpp
@@ -0,0 +1,52 @@
+// selection sort
+#include <bits/stdc++.h>
+
+using namespace std;
+void bubblesort(int arr[], int n)
+{
+    bool swapped;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped=true;
+            }
+            
+        }
+        if(!swapped){
+            break;
+        }
+    }
+}
+// void selectionsort(int arr[], int n)
+// {
+//     int min;
+//     for (int i = 0; i < n; i++)
+//     {
+//         min = i;
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (arr[j] < arr[min])
+//             {
+//                 min = j;
+//             }
+//         }
+//         swap(arr[min], arr[i]);
+//     }
+// }
+int main()
+{
+    int n = 5;
+    int arr[n] = {4, 8, 15, 12, 7};
+    // selectionsort(arr, n);
+    bubblesort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
