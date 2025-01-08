b058e8023603457010ddb4f642bc179e2f7851b0
JAYash9 <jayash.shrivastav@gmail.com>
2025-01-08 11:59:57 +0530

    sorting techniques addong

--- queue.cpp
+++ queue.cpp
@@ -1,29 +1,59 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-class Queue{
-int *arr;
-int size;
-int front;
-int rear;
+class Queue
+{
+    int *arr;
+    int size;
+    int front;
+    int rear;
+
 public:
-Queue(int size){
-    arr = new int[size];
-    this->size = size;
-    front = -1;
-    rear = -1;  
-}
-void enqueue(int data){
-    if(rear==size){
-        cout<<"queue overflow"<<endl;
+    Queue(int size)
+    {
+        arr = new int[size];
+        this->size = size;
+        front = -1;
+        rear = -1;
     }
-    else{
-        arr[rear]=data;
-        rear++;
+    void enqueue(int data)
+    {
+        if (rear == size)
+        {
+            cout << "queue overflow" << endl;
+        }
+        else
+        {
+            arr[rear] = data;
+            rear++;
+        }
     }
-}
 };
-int main(){
+class circularQueue
+{
+    int *arr;
+    int size;
+    int front;
+    int rear;
+
+public:
+    circularQueue(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        front = rear = -1;
+    }
+    void enqueue(int data){
+        if((front==0 && rear==size-1)||rear==((front-1)%(size-1))){
+            cout<<"Queue is full"<<endl;
+        }
+        else if(front==-1){
+        
+        }
+    }
+};
+int main()
+{
     Queue q(5);
     q.enqueue(1);
     return 0;
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,159 +1,29 @@
-// build tree
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
-class Node
-{
-public:
-    int data;
-    Node *left;
-    Node *right;
-    Node(int d)
-    {
-        this->data = d;
-        this->left = NULL;
-        this->right = NULL;
-    }
-};
-// Node *buildtree(Node *root)
-// {
-//     cout << "enter the data you want to insert" << endl;
-//     int data;
-//     cin >> data;
-//     // create a new Node with class
-//     root = new Node(data);
-//     // base case
-//     if (data == -1)
-//     {
-//         return NULL;
-//     }
-//     cout << "if you want to insert in left" << endl;
-//     root->left = buildtree(root->left);
-//     // cout<<data<<endl;
-//     cout << "if you wwant to insert in right" << endl;
-//     // it will still execut if user enter -1
-//     //     buildtree(root)
-//     //   |
-//     //   |-- buildtree(root->left)
-//     //   |    |
-//     //   |    |-- buildtree(root->left->left)
-//     //   |    |    |
-//     //   |    |    |-- User enters -1, returns NULL
-//     //   |    |
-//     //   |    |-- buildtree(root->left->right)  This is where we are now
-//     // in this order
-//     root->right = buildtree(root->right);
-//     // cout<<data<<endl;
-//     return root;
-// }
-// level by level treversal
-void treetraversal(Node *root)
-{
-    // base case in recursion
-    if (root == NULL)
-    {
-        return;
-    }
-    // it is used to store node pointer in queue
-    queue<Node *> q;
-    // we store root value in node
-    q.push(root);
-    // check if q is not empty
-    while (!q.empty())
-    {
-        // store the first value of root  and process it with children
-        Node *temp = q.front();
-        // remove the element in q as we store its value in temp
-        q.pop();
-        // print the value
-        cout << temp->data << " ";
-        // check if temp left node value is not null we are processing the value
-        if (temp->left != NULL)
-        {
-            // store the root left children in queue
-            q.push(temp->left);
-        }
-        // store the root right children in queue
-        if (temp->right != NULL)
-        {
-            q.push(temp->right);
-        }
-    }
-}
-// inorder traversal with Left node then prin node and then go to right
-void inorder(Node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    inorder(root->left);
-    cout << root->data << " ";
-    inorder(root->right);
-}
-// preorder traversal
-void preorder(Node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    cout << root->data << " ";
-    preorder(root->left);
-    preorder(root->right);
-}
-// postorder
-void postorder(Node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    postorder(root->left);
-    postorder(root->right);
-    cout << root->data << " ";
-}
-void levelordertreebuild(Node *root)
-{
-   
-    queue<Node *> q;
-    cout<<"enter the data for root"<<endl;
-    int data;
-    cin>>data;
-    root = new Node(data);
-    q.push(root);
-    while (!q.empty())
-    {
-        Node *temp = q.front();
-        q.pop();
-        cout << "Enter the data for left" << temp->data << endl;
-        int leftdata;
-        cin >> leftdata;
-        if (leftdata != -1)
-        {
-            temp->left = new Node(leftdata);
-            q.push(temp->left);
-        }
-        cout << "Enter the data for right" << temp->data << endl;
-        int rightdata;
-        cin >> rightdata;
-        if (rightdata != -1)
-        {
-            temp->right = new Node(rightdata);
-            q.push(temp->right);
-        }
-    }
-}
+
 int main()
 {
-    Node *root = NULL;
-    // root = buildtree(root);
-    // 1 3 7 11 5 17
-    //  treetraversal(root);
-    //  inorder(root);
-    //  preorder(root);
-    levelordertreebuild(root);
-    postorder(root);
-
+    int n = 4;
+    int arr[n] = {12, 15, 7, 5};
+    int mini;
+    for (int i = 0; i < n - 1; i++)
+    {
+        mini = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[j] < arr[mini])
+            {
+                mini = j;
+            }
+        }
+        swap(arr[mini], arr[i]);
+        
+    }
+     for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
