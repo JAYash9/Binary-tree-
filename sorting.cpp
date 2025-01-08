08da8eb99891cd4675426ba8d10082bf94558666
JAYash9 <jayash.shrivastav@gmail.com>
2025-01-08 15:30:56 +0530

    quick sort added

--- sorting.cpp
+++ sorting.cpp
@@ -2,18 +2,89 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void insertionsort(int arr[],int n){
-    for (int  i = 0; i <= n-1; i++)
-    {
-        int j=i;
-        while (j>0 && arr[j-1]>arr[j])
-        {
-            swap(arr[j],arr[j-1]);
+int partionindex(int arr[], int low, int high)
+{
+    int pivot=arr[low];
+    int i=low;
+    int j=high;
+    while(i<j){
+        while(arr[i]<=pivot&& i<=high-1){
+            i++;
+        }
+        while(arr[j]>pivot&& j>=low+1){
             j--;
         }
-        
+        if(i<j){
+            swap(arr[i],arr[j]);
+        }
     }
-    
+    swap(arr[low],arr[j]);
+    return j;
+}
+void quicksort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partionindex(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+void merge(int arr[], int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] < arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for (int i = 0; i < temp.size(); i++)
+    {
+        arr[low + i] = temp[i];
+    }
+}
+void mergersort(int arr[], int low, int high)
+{
+    int mid = (low + high) / 2;
+    if (low > high)
+    {
+        return;
+    }
+    mergersort(arr, low, mid - 1);
+    mergersort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+void insertionsort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j], arr[j - 1]);
+            j--;
+        }
+    }
 }
 void bubblesort(int arr[], int n)
 {
@@ -25,37 +96,43 @@ void bubblesort(int arr[], int n)
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
-                swapped=true;
+                swapped = true;
             }
-            
         }
-        if(!swapped){
+        if (!swapped)
+        {
             break;
         }
     }
 }
-// void selectionsort(int arr[], int n)
-// {
-//     int min;
-//     for (int i = 0; i < n; i++)
-//     {
-//         min = i;
-//         for (int j = i + 1; j < n; j++)
-//         {
-//             if (arr[j] < arr[min])
-//             {
-//                 min = j;
-//             }
-//         }
-//         swap(arr[min], arr[i]);
-//     }
-// }
+void selectionsort(int arr[], int n)
+{
+    int min;
+    for (int i = 0; i < n; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        swap(arr[min], arr[i]);
+    }
+}
 int main()
 {
     int n = 5;
     int arr[n] = {4, 8, 15, 12, 7};
+    int low = 0;
+    int high = n - 1;
+    quicksort(arr,low,high);
+
+    // mergersort(arr, low, high);
+    // insertionsort(arr, n);
+    // bubblesort(arr, n);
     // selectionsort(arr, n);
-    insertionsort(arr, n);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
